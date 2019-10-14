9c29f2774154e624c677d123e5b656b066a0d781
tzvik15 <tzvik15@yahoo.com>
2019-10-14 11:09:35 -0600

    created starter files, linked them and jquery and boodstrap CDN's, pseudo-coded JS functions, created input field

---
+++ script.js
@@ -0,0 +1,21 @@
+//global variables
+
+//possibly (bonus, if time) a function that retrieves the user's location and displays weather for it
+var bonus = function () {}
+
+//a click event that ties to the submit button and launches several functions 
+
+//a function that stores the inputes city name in a variable, and as a localStorage item
+var store = function() {}
+
+//a function that sends an AJAX request to the weatherApi, with the stored data variable as the location (the variables for this search will be defined in the click event)
+var send = function() {}
+
+//a promise function (.then) that parses through the object received from the API (possibly storing it as a variable first), and dynamically adds the resaults to the page, specifically the name, date, temperature, humidity, UV index, wind speed, and an image depicting current weather
+var diplayInfo = function() {}
+
+//a function that retrieves the location names stored in localStorage and appends them to the display. This will include adding an "on click" event listener to any new buttons appended this way
+var pastSearch = function() {}
+
+//a function that collects future forcast information from the retrieved object (this possibly will also need to by a promisory function, TBD), and dynamically displays a weather image with the following information: date, temperature, humidity
+var future = function() {}
